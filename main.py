7606b75377ee9c0fc6bed23cd5549aee3424c4c4
ayoubabbas27 <abbasayoub2703@gmail.com>
2025-03-27 17:20:04 +0100

    v1

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+from layer_dense import Layer_Dense
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, 1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+layer1 = Layer_Dense(sample_size=4, neurones_number=5)
+layer2 = Layer_Dense(sample_size=5, neurones_number=2)
+
+layer1.forward(X)
+print(layer1.weights)
+# print(layer1.output)
+layer2.forward(layer1.output)
+# print(layer2.output)
